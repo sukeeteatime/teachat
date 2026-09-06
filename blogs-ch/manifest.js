b22d4756d68e55e476fbf51e62dbc3f2d6f0bdab
Sukee Parker <Sukee.Parker@gmail.com>
2026-09-06 18:02:59 -0500

    Publish 1 new, 1 updated article(s) — 2026-09-06

--- blogs-ch/manifest.js
+++ blogs-ch/manifest.js
@@ -1,3 +1,4 @@
 window.BLOG_MANIFEST_ZH = [
-  "2023-06-18-understand-minimalism.js"
+  "2023-06-18-understand-minimalism.js",
+  "2026-09-06-ten-reasons-to-leave-austin-texas.js"
 ];
--- blogs-ch/meta.js
+++ blogs-ch/meta.js
@@ -3,6 +3,11 @@ window.BLOG_META_ZH = {
   "tags": [],
   "posts": [
     {
+      "id": "2026-09-06-ten-reasons-to-leave-austin-texas",
+      "title": "奥斯汀生活｜离开奥斯汀的十个理由",
+      "date": "2026-09-06"
+    },
+    {
       "id": "2023-06-18-understand-minimalism",
       "title": "生活的艺术流派｜极简主义",
       "date": "2023-06-18"
