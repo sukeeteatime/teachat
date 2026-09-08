e03bba7e26debcc58c76d282763c25ad35d1250d
Sukee Parker <Sukee.Parker@gmail.com>
2026-09-07 19:32:06 -0500

    Publish 1 new article(s) — 2026-09-08-web-authentication-single-sign-on-sso

--- blogs/manifest.js
+++ blogs/manifest.js
@@ -404,5 +404,6 @@ window.BLOG_MANIFEST = [
   "2026-09-07-web-authentication-fully-explained.js",
   "2026-09-07-web-authentication-part-2.js",
   "2026-09-07-web-development-fundamental-understanding-git.js",
-  "2026-09-07-why-you-would-work-with-the-team.js"
+  "2026-09-07-why-you-would-work-with-the-team.js",
+  "2026-09-08-web-authentication-single-sign-on-sso.js"
 ];
