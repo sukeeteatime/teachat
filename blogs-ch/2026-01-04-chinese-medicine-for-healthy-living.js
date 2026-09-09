86865501c17ceae49dfad80879b88c292414d445
Sukee Parker <Sukee.Parker@gmail.com>
2026-09-09 18:58:53 -0500

    Update 1 article(s) — 2026-01-04-chinese-medicine-for-healthy-living

--- blogs-ch/2026-01-04-chinese-medicine-for-healthy-living.js
+++ blogs-ch/2026-01-04-chinese-medicine-for-healthy-living.js
@@ -9,7 +9,7 @@ window.BLOG_REGISTRY_ZH.push({
   "excerpt": "中医注重养生",
   "content": "<div class=\"slideshow-block\"><div class=\"ss-slide active\"><img src=\"blogs-ch/images/2026-01-04-chinese-medicine-for-healthy-living_img1.png\" alt=\"Slide 1\"></div><div class=\"ss-slide\"><img src=\"blogs-ch/images/2026-01-04-chinese-medicine-for-healthy-living_img2.png\" alt=\"Slide 2\"></div><div class=\"ss-slide\"><img src=\"blogs-ch/images/2026-01-04-chinese-medicine-for-healthy-living_img3.png\" alt=\"Slide 3\"></div><div class=\"ss-slide\"><img src=\"blogs-ch/images/2026-01-04-chinese-medicine-for-healthy-living_img4.png\" alt=\"Slide 4\"></div><button class=\"ss-prev\" aria-label=\"Previous\">‹</button><button class=\"ss-next\" aria-label=\"Next\">›</button><div class=\"ss-dots\"><span class=\"ss-dot active\"></span><span class=\"ss-dot\"></span><span class=\"ss-dot\"></span><span class=\"ss-dot\"></span></div></div>",
   "contentType": "html",
-  "autoRead": true,
+  "autoRead": false,
   "pinned": false,
   "showHome": true
 });
