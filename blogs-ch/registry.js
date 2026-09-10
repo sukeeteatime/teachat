5e03a0133626fd0f41480248cd58e10d75545c9a
Sukee Parker <Sukee.Parker@gmail.com>
2026-09-10 11:36:09 -0500

    Publish 1 new article(s) — 2026-03-31-2026-trip-to-japan-for-the-cherry-blossom

--- blogs-ch/registry.js
+++ blogs-ch/registry.js
@@ -58,6 +58,23 @@ window.BLOG_REGISTRY_ZH.push({
   "showHome": true
 });
 window.BLOG_REGISTRY_ZH.push({
+  "id": "2026-03-31-2026-trip-to-japan-for-the-cherry-blossom",
+  "title": "2026 日本樱花之旅",
+  "author": "Sukee Parker",
+  "date": "2026-03-31",
+  "category": "蔓行游记",
+  "tags": [
+    "日本",
+    "鲜花旅行"
+  ],
+  "excerpt": "樱花的魅力就在于它犹如人的生命—那灿烂却是转瞬即逝。",
+  "content": "<p><span>鲜花宛如自然生命中的珍宝——美丽而且有着寓意深刻的存在。从阿姆斯特丹的郁金香，到日本的紫藤和樱花，再到法国的薰衣草和向日葵，德州温和优雅的蓝帽花和加州艳丽的罂粟花…花开的时候，争奇斗艳，锐意表达，只有置身于其中才可以体会真切。于是，很多年前就把鲜花之旅加入到了旅行愿望之中，和海滩、博物馆和世界文化遗产一起慢慢收集。 </span><span>&nbsp;</span></p><p>在鲜花之旅的计划中，看樱花一直占据着首要且不可或缺的位置。樱花的魅力就在于它犹如人的生命—那灿烂却是转瞬即逝。世界给我们有限时空，却给我们无限的可能而去追逐和探寻。留不住的美景<span>和可以永存心间的那份感动，这也许着就是大多人不舍离开的原因吧。</span></p><p>这次日本的樱花之旅，让我看到的生命的真实<span>——</span>没有绝对完美只有充满遗憾的释然。<span> </span>我们只需尽力做的最好，然后把结果交给天意。当我对结果不在执着，生命才最终达到无我的自由。</p>",
+  "contentType": "html",
+  "autoRead": true,
+  "pinned": false,
+  "showHome": true
+});
+window.BLOG_REGISTRY_ZH.push({
   "id": "2026-04-24-travel-to-nanjing-china",
   "title": "旧巷古城逛南京",
   "author": "Sukee Parker",
